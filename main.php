2ff56d67d9485a9c74181cbe7d5261a733833272
wissagamma <114036793+wissagamma@users.noreply.github.com>
2024-08-07 13:01:16 +0700

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Info Paket XL</title>
+    <title>Info Paket</title>
     <style>
         body {
             font-family: "Roboto", sans-serif;
